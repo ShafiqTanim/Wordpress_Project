80b5df97b5a589c53b8745a5879e833d5176b002
ShafiqTanim <shafiqullah715@gmail.com>
2024-08-31 14:04:57 +0600

    nav menu

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
 register_nav_menus( array(
-    'primary' => __( 'Top primary menu', 'ims' ),
-    'secondary' => __( 'Secondary menu', 'ims' ),
+    'primary' => __( 'Top primary menu', 'blogy' ),
+    'secondary' => __( 'Secondary menu', 'blogy' ),
     ) );
